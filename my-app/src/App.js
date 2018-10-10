e29a277b1a2ab3f77edd0d98fdb42fede47b84a8
Brittney <brittfort34@gmail.com>
2018-10-10 06:42:44 +0000

    Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
+// Parent component, Ninja is child component
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas />
+        <Ninjas name = "Sally" age = "28" belt = "yellow" />
+        <Ninjas name = "Yoshi" age = "30" belt = "black" />
       </div>
     );
   }
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
     render() {
+        // console.log(this.props);
+        // destructure -- grab the property names
+        // and save them to variables
+        const { name, age, belt } = this.props;
+        
         return (
             <div className = "ninja">
-                <div>Name: Ryu</div>
-                <div>Age: 30</div>
-                <div>Belt: Black</div>
+                <div>Name: { name }</div>
+                <div>Age: { age }</div>
+                <div>Belt: { belt }</div>
             </div>
         )
     }
