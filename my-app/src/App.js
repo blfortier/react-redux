9eaec5aa3080ecf2bfc7c28e8b24284dcdb02f5d
Brittney <brittfort34@gmail.com>
2018-10-10 07:00:10 +0000

    Outputting Lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,19 @@ import Ninjas from './Ninjas';
 
 // Parent component, Ninja is child component
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas name = "Sally" age = "28" belt = "yellow" />
-        <Ninjas name = "Yoshi" age = "30" belt = "black" />
+        <Ninjas ninjas = { this.state.ninjas }/>
       </div>
     );
   }
